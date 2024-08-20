5e0fa3297769aeffdf52b4a8e40aec92889768e7
msolfritz <msolfritz@gmail.com>
2024-08-20 17:10:39 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,8 +1,7 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
-    full_name = f"{first_name}{last_name}"
-    print(full_name.lower())
-    print(full_name.title())
-    print(full_name.upper())
-    print(f"\t{full_name.lower()}")
+    print(first_name.lower()+""+last_name.lower())
+    print(first_name.title()+""+last_name.title())
+    print(first_name.upper()+""+last_name.upper())
+    print("\t"+first_name.lower()+""+last_name.lower())
