65996585e7d79cb99f221ef7e1f6acde070c41e4
msolfritz <msolfritz@gmail.com>
2024-08-20 19:45:56 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,13 +1,21 @@
-def cambio():
-    Gasto = 23.75
-    Dinero = 100
-
-    Vuelto = Dinero - Gasto
-
-    Pesos = int(Vuelto)
-    Centavos = int((Vuelto - Pesos)*100)
-
-    print("\nVuelto\n")
-    print(f"Pesos:\n{Pesos}")
-    print(f"Centavos:\n{Centavos}")
+def change():
+    gasto: 23.75
+    Dinero_recibido: 100
     
+    Pesos: 76
+    Centavos: 25
+
+    gasto = float ("23.75\n")
+    Dinero_recibido = float ("100\n")
+    vuelto = Dinero_recibido - gasto
+    pesos = int(vuelto)
+    centavos = int(round((vuelto - pesos)*100))
+
+    print("Ingresar gasto:")
+    print(gasto)
+    print("Dinero recibido")
+    print(int(Dinero_recibido))
+
+    print (" \nVuelto\n")
+    print (f"Pesos: \n{pesos}")
+    print (f"Centavos: \n{centavos}")
